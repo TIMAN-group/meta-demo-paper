9cd466c2380d52c5b6718948a01dee339d5844b0
Sean Massung <massung1@illinois.edu>
2016-03-29 19:45:12 -0500

    improve lemur indexing code

--- indri-5.9/my-buildindex/index.cpp
+++ indri-5.9/my-buildindex/index.cpp
@@ -9,8 +9,8 @@
 
 std::vector<std::string> get_stopwords()
 {
-    std::cout << "Load stopwords" << std::endl;
-    std::ifstream in{"/home/sean/projects/meta/data/lemur-stopwords.txt"};
+    std::cerr << "Load stopwords" << std::endl;
+    std::ifstream in{"lemur-stopwords.txt"};
     std::vector<std::string> stopwords;
     std::string word;
     while (in >> word)
@@ -21,7 +21,7 @@ std::vector<std::string> get_stopwords()
 void setup_index_env(indri::api::IndexEnvironment& ienv,
                      const std::string& index_name, size_t memory)
 {
-    std::cout << "Setup index env" << std::endl;
+    std::cerr << "Setup index env" << std::endl;
     ienv.create(index_name);
     ienv.setMemory(memory);
     ienv.setStemmer("porter");
@@ -32,7 +32,7 @@ void setup_index_env(indri::api::IndexEnvironment& ienv,
 void create_line_index(indri::api::IndexEnvironment& ienv, size_t memory,
                        const std::string& dataset, const std::string& prefix)
 {
-    std::cout << "Create line index" << std::endl;
+    std::cerr << "Create line index" << std::endl;
 
     std::string name = dataset + "-index";
     setup_index_env(ienv, name, memory);
@@ -40,7 +40,7 @@ void create_line_index(indri::api::IndexEnvironment& ienv, size_t memory,
     size_t num_indexed = 0;
     std::string line;
     auto dataset_path = prefix + "/" + dataset + ".dat";
-    std::cout << "dataset path: " << dataset_path << std::endl;
+    std::cerr << "dataset path: " << dataset_path << std::endl;
     std::ifstream docs{dataset_path};
     std::ifstream name_file{prefix + "/" + "/metadata.dat"};
     while (docs.good())
@@ -55,7 +55,7 @@ void create_line_index(indri::api::IndexEnvironment& ienv, size_t memory,
         std::getline(docs, line);
         ienv.addString(line, "text", {mdata});
         ++num_indexed;
-        if (num_indexed % 1000 == 0)
+        if (num_indexed % 10000 == 0)
         {
             std::cout << " Indexed " << num_indexed << " docs...    \r";
             cout.flush();
@@ -63,7 +63,7 @@ void create_line_index(indri::api::IndexEnvironment& ienv, size_t memory,
     }
 
     ienv.close();
-    std::cout << std::endl;
+    std::cerr << std::endl;
 }
 
 int main(int argc, char* argv[])
@@ -84,12 +84,12 @@ int main(int argc, char* argv[])
     indri::api::IndexEnvironment ienv;
     if (build_index)
     {
-        std::string prefix = "/home/sean/data/" + dataset;
+        std::string prefix = "/media/" + dataset;
         create_line_index(ienv, memory, dataset, prefix);
     }
     auto end = std::chrono::steady_clock::now();
     auto time = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-    std::cout << "Index creation time: " << time.count() << " seconds."
+    std::cerr << "Index creation time: " << time.count() << " seconds."
               << std::endl;
 
     // run queries on index
@@ -118,7 +118,7 @@ int main(int argc, char* argv[])
         for (auto& name : names)
         {
             std::cout << query_id << "\t_\t" << name << "\t_\t"
-                      << results[res].score << "\t_" << std::endl;
+                      << results[res].score << "\t_\n";
             ++res;
         }
         ++query_id;
@@ -128,5 +128,5 @@ int main(int argc, char* argv[])
     end = std::chrono::steady_clock::now();
     auto rtime
         = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    std::cout << "Retrieval time: " << rtime.count() << "ms" << std::endl;
+    std::cerr << "Retrieval time: " << rtime.count() << "ms" << std::endl;
 }
